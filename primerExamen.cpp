0d00419edd7e5da91af0ff3503bd502bad101a8a
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2018-09-26 08:34:55 -0500

    Examen

---
+++ primerExamen.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int *arreglo = new int [5];
+
+	for(int i=0;i<5;i++){
+		cout<<"Digite un numero ["<<i<<"]: ";
+		cin>>*(arreglo+i);
+	}
+    cout << endl;
+	for(int i=0;i<5;i++){
+		cout << "valor:    ";
+		cout << *(arreglo+i) << endl;
+		cout << "posicion: " << (arreglo+i)<< endl;
+	}
+
+    delete [] arreglo;
+	return 0;
+}
