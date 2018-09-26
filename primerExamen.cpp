22c5b4c75c30845fafaf12edf781253110ad4955
GuztavoDuran <42410146+GuztavoDuran@users.noreply.github.com>
2018-09-26 08:49:45 -0500

    Update primerExamen.cpp

--- primerExamen.cpp
+++ primerExamen.cpp
@@ -13,6 +13,12 @@ int main(){
 		cout << *(arreglo+i) << endl;
 		cout << "posicion: " << (arreglo+i)<< endl;
 	}
+	
+	for(int i=4;i>=0;i--){
+		cout << "valor:    ";
+		cout << *(arreglo+i) << endl;
+		cout << "posicion: " << (arreglo+i)<< endl;
+	}
 
     delete [] arreglo;
 	return 0;
